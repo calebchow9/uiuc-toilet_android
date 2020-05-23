773ed6764d91f2782601cb97a1148e462d1aea88
Caleb Chow <calebchow9@gmail.com>
2020-05-23 16:13:06 -0400

    implementing bathroom cardview, finished get volley request

--- app/src/main/java/com/example/uiuc_toilet_android/ListActivity.java
+++ app/src/main/java/com/example/uiuc_toilet_android/ListActivity.java
@@ -2,6 +2,7 @@ package com.example.uiuc_toilet_android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
@@ -10,17 +11,29 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static android.location.Location.distanceBetween;
 import static com.example.uiuc_toilet_android.MapsActivity.latitude;
 import static com.example.uiuc_toilet_android.MapsActivity.longitude;
 
 public class ListActivity extends AppCompatActivity {
+    private String BASE_URL = "http://192.168.3.10:3000";
     RecyclerView recyclerView;
     ListAdapter adapter;
     RecyclerView.LayoutManager layoutManager;
@@ -50,6 +63,8 @@ public class ListActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        getBathrooms(latitude, longitude);
     }
 
     private void buildRecyclerView() {
@@ -61,9 +76,46 @@ public class ListActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
-    private void getBathrooms(){
-        float[] distance = new float[1];
-        //distanceBetween(latitude, longitude, geolocation.getDouble("latitude"), geolocation.getDouble("longitude"), distance);
-        double locationDistance = (double) distance[0];
+    private void getBathrooms(final double latitude, final double longitude){
+        RequestQueue queue;
+        queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest stringRequest = new JsonArrayRequest(com.android.volley.Request.Method.GET, BASE_URL+"/bathroom", null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("info", response.toString());
+                        brList.clear();
+                        try{
+                            for(int i = 0; i < response.length(); i++){
+                                JSONObject br = response.getJSONObject(i);
+                                String id = br.getString("_id");
+                                String name = br.getString("name");
+                                String gender = br.getString("gender");
+                                double brLatitude = br.getDouble("latitude");
+                                double brLongitude = br.getDouble("longitude");
+                                double openTime = br.getDouble("openTime");
+                                double closeTime = br.getDouble("closeTime");
+                                float[] distance = new float[1];
+                                distanceBetween(latitude, longitude, brLatitude, brLongitude, distance);
+                                double locationDistance = distance[0];
+                                Bathroom bathroom = new Bathroom (id, name, gender, brLatitude, brLongitude, openTime, closeTime, locationDistance);
+                                brList.add(bathroom);
+                            }
+                            adapter.notifyDataSetChanged();
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        queue.add(stringRequest);
     }
 }
--- app/src/main/java/com/example/uiuc_toilet_android/ListAdapter.java
+++ app/src/main/java/com/example/uiuc_toilet_android/ListAdapter.java
@@ -1,13 +1,16 @@
 package com.example.uiuc_toilet_android;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -19,11 +22,15 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.BRViewHolder> 
     public static class BRViewHolder extends RecyclerView.ViewHolder {
         TextView name;
         TextView distance;
+        ImageView male;
+        ImageView female;
 
         public BRViewHolder(View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.bathroom_name);
             distance = itemView.findViewById(R.id.distance);
+            male = itemView.findViewById(R.id.ic_male);
+            female = itemView.findViewById(R.id.ic_female);
         }
 
     }
@@ -40,10 +47,23 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.BRViewHolder> 
 
     @Override
     public void onBindViewHolder(BRViewHolder holder, final int position) {
+        Resources res = holder.itemView.getContext().getResources();
         Bathroom currentItem = brList.get(position);
 
         holder.name.setText(currentItem.getName());
         holder.distance.setText(parseDistance(currentItem.getDistanceFromUser()));
+
+        switch(currentItem.getGender()){
+            case "male":
+                holder.male.setColorFilter(res.getColor(R.color.male));
+                break;
+            case "female":
+                holder.female.setColorFilter(res.getColor(R.color.female));
+            case "both":
+                holder.male.setColorFilter(res.getColor(R.color.male));
+                holder.female.setColorFilter(res.getColor(R.color.female));
+            default:
+        }
 
         //on click, open specific SpaceActivity page
 //        holder.itemView.setOnClickListener(new View.OnClickListener() {
